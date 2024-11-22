530ce6de735df1dd37b2259a944c7d5bd00e3b57
MrDevas <devaswork1111@gmail.com>
2024-11-22 11:46:18 +0530

    first commit

---
+++ src/Components/Cert2.tsx
@@ -0,0 +1,11 @@
+import Cert from '../assets/Images/cert.jpg';
+import "../Components/Portfolio.css";
+export function Cert2(){
+    return(
+        <>
+        <div className="certification">
+        <img src= {Cert}></img>
+        </div>
+        </>
+    )
+}
